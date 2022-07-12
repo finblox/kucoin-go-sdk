8d64b9a221defd73a9e3df379b4ca58e0ed44703
Sarfaraz Nawaz <sir_nawaz959@yahoo.com>
2022-07-12 20:28:28 +0530

    Avoid using http.DefaultClient

--- http.go
+++ http.go
@@ -136,7 +136,7 @@ func (br *BasicRequester) Request(request *Request, timeout time.Duration) (*Res
 		tc.InsecureSkipVerify = request.SkipVerifyTls
 	}
 
-	cli := http.DefaultClient
+	cli := http.Client{}
 	cli.Transport, cli.Timeout = tr, timeout
 
 	req, err := request.HttpRequest()
